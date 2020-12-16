c341f627ca126550a5b0d92188a050a0a2edbf63
Dmitry <mamhigtt@gmail.com>
2020-12-17 01:03:45 +0700

    update lexer

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -1,228 +1,222 @@
-use std::str::CharIndices;
+use sana::{Spanned, Sana};
 
-pub enum Tok {
+#[derive(Debug)]
+pub struct ParserError;
+
+type ParserItem = Result<(usize, Token, usize), ParserError>;
+
+pub fn token_iter<'input>(input: &'input str) -> impl Iterator<Item=ParserItem> + 'input {
+    Token::lexer(input)
+        .filter(|tok| tok.value != Token::Whitespace)
+        .map(|tok| match tok {
+            Spanned { value: Token::Error, .. } =>
+                Err(ParserError),
+            Spanned { value, start, end} =>
+                Ok((start, value, end)),
+        })
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Sana)]
+pub enum Token {
+    #[token("auto", priority = 1)]
     Auto,
+    #[token("break", priority = 1)]
     Break,
+    #[token("case", priority = 1)]
     Case,
+    #[token("char", priority = 1)]
     Char,
+    #[token("const", priority = 1)]
     Const,
+    #[token("continue", priority = 1)]
     Continue,
+    #[token("default", priority = 1)]
     Default,
+    #[token("do", priority = 1)]
     Do,
+    #[token("double", priority = 1)]
     Double,
+    #[token("else", priority = 1)]
     Else,
+    #[token("enum", priority = 1)]
     Enum,
+    #[token("extern", priority = 1)]
     Extern,
+    #[token("float", priority = 1)]
     Float,
+    #[token("for", priority = 1)]
     For,
+    #[token("goto", priority = 1)]
     Goto,
+    #[token("if", priority = 1)]
     If,
+    #[token("inline", priority = 1)]
     Inline,
+    #[token("int", priority = 1)]
     Int,
+    #[token("long", priority = 1)]
     Long,
+    #[token("register", priority = 1)]
     Register,
+    #[token("restrict", priority = 1)]
     Restrict,
+    #[token("return", priority = 1)]
     Return,
+    #[token("short", priority = 1)]
     Short,
+    #[token("signed", priority = 1)]
     Signed,
+    #[token("sizeof", priority = 1)]
     Sizeof,
+    #[token("static", priority = 1)]
     Static,
+    #[token("struct", priority = 1)]
     Struct,
+    #[token("switch", priority = 1)]
     Switch,
+    #[token("typedef", priority = 1)]
     Typedef,
+    #[token("union", priority = 1)]
     Union,
+    #[token("unsigned", priority = 1)]
     Unsigned,
+    #[token("void", priority = 1)]
     Void,
+    #[token("volatile", priority = 1)]
     Volatile,
+    #[token("while", priority = 1)]
     While,
+    #[token("_Alignas", priority = 1)]
     Alignas,
+    #[token("_Alignof", priority = 1)]
     Alignof,
+    #[token("_Atomic", priority = 1)]
     Atomic,
+    #[token("_Bool", priority = 1)]
     Bool,
+    #[token("_Complex", priority = 1)]
     Complex,
+    #[token("_Generic", priority = 1)]
     Generic,
+    #[token("_Imaginary", priority = 1)]
     Imaginary, 
+    #[token("_Noreturn", priority = 1)]
     Noreturn,
+    #[token("_Static_assert", priority = 1)]
     StaticAssert,
+    #[token("_Thread_local", priority = 1)]
     ThreadLocal,
 
+    #[regex("[a-zA-Z_][a-zA-Z0-9_]*")] 
     Identifier,
+
+    #[regex("[0-9]+")]
     Constant,
+
+    #[regex("\"\"")]
     StringLiteral,
-    //Punctuators
-    LeftSquareBracket,
-    RightSquaredBracket,
-    LeftCurvedParenthesis,
-    RightCurvedParenthesis,
-    LeftCurlyBrace,
-    RightCurlyBrace,
+
+    // TypedefName,
+    #[token("[")]
+    LBracket,
+    #[token("]")]
+    RBracket,
+    #[token("(")]
+    LParenthesis,
+    #[token(")")]
+    RParenthesis,
+    #[token("{")]
+    LBrace,
+    #[token("}")]
+    RBrace,
+    #[token(".")]
     Dot,
+    #[token("->")]
     Arrow,
+    #[token("++")]
     TwoPluses,
+    #[token("--")]
     TwoMinuses,
+    #[token("&")]
     Ampersand,
-    Star,
+    #[token("*")]
+    Asterisk ,
+    #[token("+")]
     Plus,
+    #[token("-")]
     Minus,
-    Wave,
+    #[token("~")]
+    Tilde,
+    #[token("!")]
     ExclamationMark,
+    #[token("/")]
     Slash,
+    #[token("%")]
     Percent,
+    #[token("<<")]
     TwoSmaller,
+    #[token(">>")]
     TwoLager,
+    #[token("<")]
     Smaller,
+    #[token(">")]
     Lager,
+    #[token("<=")]
     LeOrEq,
+    #[token(">=")]
     GeOrEq,
+    #[token("==")]
     Equal,
+    #[token("!=")]
     NotEq,
-    CheckMark,
-    VerticalLine,
-    TwoAmpersand,
-    TwoVerticalLine,
+    #[token("^")]
+    Caret,
+    #[token("|")]
+    Bar,
+    #[token("&&")]
+    WAmpersand,
+    #[token("||")]
+    WBar,
+    #[token("?")]
     QuestionMark,
+    #[token(":")]
     Colon,
+    #[token(";")]
     Semicolon,
+    #[token("...")]
     ThreeDots,
-    Asign,
-    StarAsign,
-    SlashAsign,
-    VerticalAsign,
+    #[token("=")]
+    Assign,
+    #[token("*=")]
+    AsteriskAssign,
+    #[token("/=")]
+    SlashAssign,
+    #[token("%=")]
+    PercentAssign,
+    #[token("+=")]
+    PlusAssign,
+    #[token("-=")]
+    MinusWHyphen,
+    #[token("<<=")]
+    LGuillemetsAssign,
+    #[token(">>=")]
+    RGuillemetsAssign,
+    #[token("&=")]
+    AmpersandAssign,
+    #[token("^=")]
+    CaretAssign,
+    #[token("|=")]
+    VBarAssign,
+    #[token(",")]
     Comma,
-    Lattice,
-    TwoLattice,
-    S1,
-    S2,
-    S3,
-    S4,
-    S5,
-    S6,
+
+    #[regex("[ \t\r\n]+")]
+    Whitespace,
+
+    #[error]
+    Error,
 }
 
-enum LexicalError {
-    
-}
-
-pub struct Lexer<'input> {
-    chars: CharIndices<'input>,
-}
-
-impl<'input> Lexer<'input> {
-    pub fn new(input: &'input str) -> Self {
-        Lexer { chars: input.char_indices() }
-    }
-}
-
-impl<'input> Iterator for Lexer<'input> {
-    type Item = Spanned<Tok, usize, LexicalError>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            match self.chars.next() {
-                Some((i, ' ')) => continue,
-                Some((i, '\t')) => continue,
-                Some((i, '\n')) => continue,
-
-                None => return None, 
-            }
-        }
-    }
-}
-
-// keyword
-
-// "auto" "break" "case" "char" "const" 
-// "continue" "default" "do" "double" "else"
-// "enum" "extern" "float" "for" "goto" "if"
-// "inline" "int" "long" "register" "restrict" "return"
-// "short" "signed" "sizeof" "static" "struct" 
-// "switch" "typedef" "union" "unsigned" "void"
-// "volatile" "while" "_Alignas" "_Alignof" "_Atomic"
-// "_Bool" "_Complex" "_Generic" "_Imaginary" "_Noreturn"
-// "_Static_assert" "_Thread_local"
 
 
-//////////////////////////////////////////////////
-/// Constants ////////////////////////////////////
-//////////////////////////////////////////////////
-
-// pub Constant: Box<expr::Expr> = {
-//     IntegerConstant => ,
-//     FloatingConstant => ,
-//     EnumerationConstant => ,
-//     CharacterConstant, 
-// };
-
-// IntegerConstant: = {
-//     decimal-constant integer-suffixopt
-//     octal-constant integer-suffixopt
-//     hexadecimal-constant integer-suffixopt
-// };
-
-// DecimalConstant: = {
-//     nonzero-digit
-//     decimal-constant digit
-// };
-
-// OctalConstant: = {
-//     "0"
-//     OctalConstant OctalDigit
-// };
-
-// HexadecimalConstant: = {
-//     HexadecimalPrefix HexadecimalDigit
-//     HexadecimalConstant HexadecimalDigit
-// };
-
-// HexadecimalPrefix: = {
-//     "0x", 
-//     "0X",
-// };
-
-// NonzeroDigit: = {
-//     "1" "2" "3" "4" "5" "6" "7" "8" "9"
-// };
-
-// OctalDigit: = {
-//     "0" "1" "2" "3" "4" "5" "6" "7"
-// };
-
-// CharacterConstantPrefix: expr::CharLiteralPrefix = {
-//     "L" => expr::CharLiteralPrefix::L,
-//     "u" => expr::CharLiteralPrefix::LCU,
-//     "U" => expr::CharLiteralPrefix::UCU, 
-// };
-
-// pub CharacterConstant: expr::CharLiteral = {
-//     <pref: CharacterConstantPrefix> "'" <val: CCharSequence> "'" 
-//         => expr::CharLiteral{prefix: Some(pref), value: val},
-//     "'" <val: CCharSequence> "'" 
-//         => expr::CharLiteral{prefix: None, value: val},
-// };
-
-// //TODO: this is a very naive implementation that only partially conforms to the standard
-// CCharSequence: u8 = <s:r"[[:alpha:]]"> => s.bytes().nth(0).unwrap();
-
-// StringLiteral: Box<expr::Expr> = 
-//     <prefix: EncodingPrefix?> "\"" <value: SCharSequence?> "\"" 
-//         => expr::Expr::Literal(expr::Literal::Str(expr::StringLiteral{prefix, value}))
-// ;
-
-// EncodingPrefix: expr::StringLiteralPrefix = {
-//     "u8" => expr::StringLiteralPrefix::U8,
-//     "u" => expr::StringLiteralPrefix::LCU,
-//     "U" => expr::StringLiteralPrefix::UCU,
-//     "L" => expr::StringLiteralPrefix::L,
-// };
-
-// SCharSequence = r"[[:alpha:]]+;
-
-// "[" "]" "(" ")" "{" "}" "." "->"
-// "++" "--" "&" "*" "+" "-" "~" '!"
-// "/" "%" "<<" ">>" "<" ">" "<=" ">="
-// "==" "!=" "^" "|" "&&" "||"
-// "?" ":" ";" "..."
-// "=" "*=" "/=" "%=" "+=" "-=" "<<="
-// ">>=" "&=" "^=" "|=" "," "#" "##"
-// "<:" ":>" "<%" "%>" "%:" "%:%:"
 
